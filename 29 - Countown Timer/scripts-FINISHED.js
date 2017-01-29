bb2c9bc8e70c8f938d1a96b7a3b7a481bc5de3a1
Christopher Comella <git.chrisco@gmail.com>
2017-01-29 15:46:50 +0100

    Beautify and Prettier

--- 29 - Countown Timer/scripts-FINISHED.js
+++ 29 - Countown Timer/scripts-FINISHED.js
@@ -12,22 +12,25 @@ function timer(seconds) {
   displayTimeLeft(seconds);
   displayEndTime(then);
 
-  countdown = setInterval(() => {
-    const secondsLeft = Math.round((then - Date.now()) / 1000);
-    // check if we should stop it!
-    if(secondsLeft < 0) {
-      clearInterval(countdown);
-      return;
-    }
-    // display it
-    displayTimeLeft(secondsLeft);
-  }, 1000);
+  countdown = setInterval(
+    () => {
+      const secondsLeft = Math.round((then - Date.now()) / 1000);
+      // check if we should stop it!
+      if (secondsLeft < 0) {
+        clearInterval(countdown);
+        return;
+      }
+      // display it
+      displayTimeLeft(secondsLeft);
+    },
+    1000
+  );
 }
 
 function displayTimeLeft(seconds) {
   const minutes = Math.floor(seconds / 60);
   const remainderSeconds = seconds % 60;
-  const display = `${minutes}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`;
+  const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
   document.title = display;
   timerDisplay.textContent = display;
 }

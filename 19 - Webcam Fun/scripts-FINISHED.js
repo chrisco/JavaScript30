1d03b36a52e67ec758ac9064595e61dc1cf1b5e4
Christopher Comella <git.chrisco@gmail.com>
2017-02-01 20:25:54 +0100

    Big Git merge

--- 19 - Webcam Fun/scripts-FINISHED.js
+++ 19 - Webcam Fun/scripts-FINISHED.js
@@ -5,14 +5,15 @@ const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
 
 function getVideo() {
-  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: false })
     .then(localMediaStream => {
       console.log(localMediaStream);
       video.src = window.URL.createObjectURL(localMediaStream);
       video.play();
     })
     .catch(err => {
-      console.error(`OH NO!!!`, err);
+      console.error('OH NO!!!', err);
     });
 }
 
@@ -22,20 +23,21 @@ function paintToCanavas() {
   canvas.width = width;
   canvas.height = height;
 
-  return setInterval(() => {
-    ctx.drawImage(video, 0, 0, width, height);
-    // take the pixels out
-    let pixels = ctx.getImageData(0, 0, width, height);
-    // mess with them
-    // pixels = redEffect(pixels);
-
-    pixels = rgbSplit(pixels);
-    // ctx.globalAlpha = 0.8;
-
-    // pixels = greenScreen(pixels);
-    // put them back
-    ctx.putImageData(pixels, 0, 0);
-  }, 16);
+  return setInterval(
+    () => {
+      ctx.drawImage(video, 0, 0, width, height);
+      // take the pixels out
+      let pixels = ctx.getImageData(0, 0, width, height);
+      // mess with them
+      // pixels = redEffect(pixels);
+      pixels = rgbSplit(pixels);
+      // ctx.globalAlpha = 0.8;
+      // pixels = greenScreen(pixels);
+      // put them back
+      ctx.putImageData(pixels, 0, 0);
+    },
+    16
+  );
 }
 
 function takePhoto() {
@@ -53,18 +55,22 @@ function takePhoto() {
 }
 
 function redEffect(pixels) {
-  for(let i = 0; i < pixels.data.length; i+=4) {
-    pixels.data[i + 0] = pixels.data[i + 0] + 200; // RED
-    pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 200;
+    // RED
+    pixels.data[i + 1] = pixels.data[i + 1] - 50;
+    // GREEN
     pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // Blue
   }
   return pixels;
 }
 
 function rgbSplit(pixels) {
-  for(let i = 0; i < pixels.data.length; i+=4) {
-    pixels.data[i - 150] = pixels.data[i + 0]; // RED
-    pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 150] = pixels.data[i + 0];
+    // RED
+    pixels.data[i + 500] = pixels.data[i + 1];
+    // GREEN
     pixels.data[i - 550] = pixels.data[i + 2]; // Blue
   }
   return pixels;
@@ -73,7 +79,7 @@ function rgbSplit(pixels) {
 function greenScreen(pixels) {
   const levels = {};
 
-  document.querySelectorAll('.rgb input').forEach((input) => {
+  document.querySelectorAll('.rgb input').forEach(input => {
     levels[input.name] = input.value;
   });
 
@@ -83,12 +89,14 @@ function greenScreen(pixels) {
     blue = pixels.data[i + 2];
     alpha = pixels.data[i + 3];
 
-    if (red >= levels.rmin
-      && green >= levels.gmin
-      && blue >= levels.bmin
-      && red <= levels.rmax
-      && green <= levels.gmax
-      && blue <= levels.bmax) {
+    if (
+      red >= levels.rmin &&
+        green >= levels.gmin &&
+        blue >= levels.bmin &&
+        red <= levels.rmax &&
+        green <= levels.gmax &&
+        blue <= levels.bmax
+    ) {
       // take it out!
       pixels.data[i + 3] = 0;
     }
